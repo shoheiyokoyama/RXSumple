aa65cd36a5e5424b90b46773c79d702bc8e07e20
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-05-26 11:21:25 +0900

    add slider

--- RXSample/ViewController.swift
+++ RXSample/ViewController.swift
@@ -10,30 +10,48 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+//UIComponentに対する拡張メソッド
+//https://github.com/ReactiveX/RxSwift/blob/master/Documentation/API.md
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var slider: UISlider!
     
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        updateLalelText()
-        buttonTapAction()
+        rxTextField()
+        rxButton()
+        rxSlider()
     }
     
-    private func updateLalelText() {
+    private func rxSlider() {
+        slider.rx_value
+            .map { String($0) }
+            .bindTo(label.rx_text)
+            .addDisposableTo(disposeBag)
+    }
+    
+    private func rxTextField() {
         textField.rx_text
             .subscribeNext { [unowned self] text in
                 self.label.text = text
             }
             .addDisposableTo(disposeBag)
+        
+        textField.rx_text
+            .filter { str in  str.characters.count > 3 }
+            .subscribeNext { str in print(str) }
+            .addDisposableTo(disposeBag)
     }
     
-    private func buttonTapAction() {
+    private func rxButton() {
         button.rx_tap
         .subscribeNext { _ in
             print("tapped!")
