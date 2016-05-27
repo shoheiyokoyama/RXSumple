0cf4a3432a6e61108ed10521d7fead015de27b17
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-05-27 14:16:03 +0900

    create playground

---
+++ RXSample/RxPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import RxSwift
+
+
+//RXSwift Example
+//https://github.com/ReactiveX/RxSwift/blob/master/Documentation/Examples.md
+
+var c: String
+var a = 1
+var b = 2
+
